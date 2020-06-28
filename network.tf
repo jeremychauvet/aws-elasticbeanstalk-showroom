e67e909891d0ef608e9e3d44a495544e56bf3a4a
Jeremy Chauvet <jeremy.chauvet@capgemini.com>
2020-06-28 13:12:09 +0200

    Add SG for application and database.

--- network.tf
+++ network.tf
@@ -15,3 +15,34 @@ module "vpc" {
     Environment = "production"
   }
 }
+
+module "app_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "app-sg"
+  description = "Allow traffic from NLB to EB."
+  vpc_id      =  "${module.vpc.vpc_id}"
+
+  ingress_cidr_blocks      = ["0.0.0.0/0"] # To be replaced by NLB.
+  ingress_rules            = ["https-443-tcp", "http-80-tcp"]
+}
+
+module "database_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "database-sg"
+  description = "Allow traffic from EB to RDS."
+  vpc_id      =  "${module.vpc.vpc_id}"
+
+  computed_ingress_with_source_security_group_id = [
+    {
+      rule                     = "mysql-tcp"
+      source_security_group_id = "${module.app_sg.this_security_group_id}"
+    }
+  ]
+  # Terraform 0.11 has a limitation which does not allow computed values inside count attribute on resources (issues: #16712, #18015, ...)
+  # Computed values are values provided as outputs from module. Non-computed values are all others - static values, values referenced as variable and from data-sources.
+  # When you need to specify computed value inside security group rule argument you need to specify it using an argument which starts with computed_ and provide a number of elements in the argument which starts with number_of_computed_.
+  # https://github.com/terraform-aws-modules/terraform-aws-security-group#note-about-value-of-count-cannot-be-computed
+  number_of_computed_ingress_with_source_security_group_id = 1
+}
