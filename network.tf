9b5a7a936f2a39fc40efc317beac2d185acefbd8
Jeremy Chauvet <jer.chauvet@gmail.com>
2020-07-04 12:41:35 +0200

    Enable multi subnet for RDS

--- network.tf
+++ network.tf
@@ -5,11 +5,12 @@ module "vpc" {
   name = "ebshowroom-vpc"
   cidr = "172.32.0.0/16"
 
-  azs             = ["eu-central-1a"]
-  public_subnets  = ["172.32.1.0/24"]
-  private_subnets = ["172.32.2.0/24"]
+  azs               = ["eu-central-1a", "eu-central-1b"]
+  public_subnets    = ["172.32.1.0/24", "172.32.2.0/24"]
+  private_subnets   = ["172.32.3.0/24", "172.32.4.0/24"]
 
   enable_nat_gateway = true
+  single_nat_gateway = true
 
   tags = {
     Terraform = "true"
