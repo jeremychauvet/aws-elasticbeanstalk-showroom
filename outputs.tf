732e128c3319077e77f300f9f734b19e55354af6
Jeremy Chauvet <jer.chauvet@gmail.com>
2020-07-04 12:43:41 +0200

    Add RDS endpoint to outputs.

--- outputs.tf
+++ outputs.tf
@@ -8,3 +8,8 @@ output "public_subnets" {
   description = "List of IDs of public subnets"
   value       = module.vpc.public_subnets
 }
+
+output "rds_endpoint" {
+  description = "Database endpoint"
+  value       = module.rds.this_db_instance_endpoint
+}
