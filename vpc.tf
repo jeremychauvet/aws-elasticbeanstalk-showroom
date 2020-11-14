be5eea187c32b602cca131a1a9b4ecceeaf20685
Jérémy Chauvet <jer.chauvet@gmail.com>
2020-11-14 11:27:19 +0100

    Use region define in parameter for AZ.

--- vpc.tf
+++ vpc.tf
@@ -5,7 +5,7 @@ module "vpc" {
   name = "ebshowroom-vpc"
   cidr = "172.32.0.0/16"
 
-  azs             = ["eu-central-1a", "eu-central-1b"]
+  azs             = ["${var.aws_region}a", "${var.aws_region}b"]
   public_subnets  = ["172.32.1.0/24", "172.32.2.0/24"]
   private_subnets = ["172.32.3.0/24", "172.32.4.0/24"]
 
