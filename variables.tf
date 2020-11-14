5bfc7636c80cbacd9235380dbda84ad224f04749
Jérémy Chauvet <jer.chauvet@gmail.com>
2020-11-14 11:17:59 +0100

    Update AWS region.

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "AWS region where ressources must be created. Will affect billing and availability."
   type        = string
-  default     = "eu-central-1"
+  default     = "us-east-1"
 }
 
 variable "aws_profile" {
